29b38253f68b9af508885ce86595b707125c74b3
Liquorshotz <jungwon1413@gmail.com>
2018-03-13 18:51:48 +0900

    Full Fix

--- make_text_by_character.py
+++ make_text_by_character.py
@@ -1,89 +1,129 @@
+import time
+import chardet
 import numpy as np
 import tensorflow as tf
-from konlpy.tag import Twitter
+
 from tensorflow.contrib import rnn
+from tensorflow.contrib.layers import fully_connected
+from tensorflow.contrib.seq2seq import sequence_loss
+
 
 # 해당 파트 구동 확인 완료되었습니다.
-class Novel_Dataset:
-    def __init__(self, full_filename, seq_len):
-        try:
-            read_file = open(full_filename, 'rb').read()
+def init_data(full_filename, encode=False):
+    try:
+        read_file = open(full_filename, 'rb').read()
+        if encode is True:
             encode_type = chardet.detect(read_file)['encoding']
-            # text = open(full_filename, encoding=None).read().lower()
             print(encode_type)
-            text = open(full_filename, encoding=encode_type).read()
-        except:
-            return
+        else:
+            encode_type = None
+        # text = open(full_filename, encoding=None).read().lower()
+        text = open(full_filename, encoding=encode_type).read()
+        return text
+    except:
+        return False
 
-        print("Word Slicing...")
-        char_set = sorted(list(set(text)))    # character split
+def slice_data(text):
+    print("Word Slicing...")
+    char_set = sorted(list(set(text)))    # character split
 
-        self.char_indices = dict((c, i) for i, c in enumerate(char_set))
-        self.indices_char = dict((i, c) for i, c in enumerate(char_set))
+    char_indices = dict((c, i) for i, c in enumerate(char_set))
+    indices_char = dict((i, c) for i, c in enumerate(char_set))
+    return char_indices, indices_char
 
-        idx_text = []
-        for i, char in enumerate(text):
-            idx_text.append(self.char_indices[char])
-        text = idx_text
+def data_to_idx(text, char_indices):
+    idx_text = []
+    for i, char in enumerate(text):
+        idx_text.append(char_indices[char])
+    text = idx_text
+    return text
 
-        print("Generating Number Index...")
-        self.x_data = []
-        self.y_data = []
-        for i in range(0, len(text) - seq_len):
-            fill_x = seq_len - len(text[i : i+seq_len])
-            fill_y = seq_len - len(text[i+1 : i+seq_len - 1])
-            
-            x_text = text[i : i+seq_len]
-            y_text = text[i+1 : i+seq_len - 1]
-            
-            if fill_x is not 0:
-                x_text.extend([0 for i in range(fill_x)])
-            elif fill_y is not 0:
-                y_text.extend([0 for i in range(fill_y)])
-
-            # Debug purpose
-            if i % 400000 is 0:
-                print('Line', i, ':', x_text, '=>', y_text)
-
-            self.x_data.append(x_text)
-            self.y_data.append(y_text)
+def build_data(text, seq_len, stride = 4):
+    print("Generating Number Index...")
+    x_data = []
+    y_data = []
+    for i in range(0, len(text) - seq_len, stride):
+        fill_x = seq_len - len(text[i : i+seq_len])
+        fill_y = seq_len - len(text[i+1 : i+seq_len - 1])
         
+        x_text = text[i : i+seq_len]
+        y_text = text[i+1 : i+seq_len - 1]
+        
+        if fill_x is not 0:
+            x_text.extend([0 for i in range(fill_x)])
+        elif fill_y is not 0:
+            y_text.extend([0 for i in range(fill_y)])
+
+        x_data.append(x_text)
+        y_data.append(y_text)
+    return x_data, y_data
+
+
+def lstm_cell(hidden_size):
+    # Make a lstm cell with hidden_size (each unit output vector size)
+    cell = rnn.BasicLSTMCell(hidden_size, state_is_tuple=True)
+    return cell
+
+def predict(result, x_data, outputs, char_indices, indices_char):
+    output = []
+    results = sess.run(outputs, feed_dict={X: x_data})
+    for j, result in enumerate(results):
+        index = np.argmax(result, axis=1)
+        if j is 0:  # print all for the first result to make a sentence
+            print(''.join([indices_char[t] for t in index]), end='')
+        else:
+            print(indices_char[index[-1]], end='')
+
+def elapsed(start, end):
+    total = end - start
+    m, s = divmod(total, 60)
+    h, m = divmod(m, 60)
+    
+    if m == 0 and h == 0:
+        print("Time Elapsed: {:>3d} sec".format(int(s)))
+    elif m != 0 and h == 0:
+        print("Time Elapsed: {:>3d} min {:>3d} sec".format(int(m), int(s)))
+    else:
+        print("Time Elapsed: {:>3d} hour {:>3d} min {:>3d} sec".format(int(h), int(m), int(s)))
+
 if __name__ == "__main__":
-    filename = "D:/Paper_txt/PAPER_01001.txt"
-    seq_len = 26
-    novel = Novel_Dataset(filename, seq_len)
+    filename = "SAMPLE.py"
+    seq_len = 40
 
-    data_dim = len(novel.characters)
-    hidden_size = len(novel.characters)
-    num_classes = len(novel.characters)
-    learning_rate = 0.001
+    # load data
+    text = init_data(filename)
+    char_indices, indices_char = slice_data(text)
+    text = data_to_idx(text, char_indices)
+    x_data, y_data = build_data(text, seq_len)
 
+    data_dim = len(char_indices)
+    hidden_size = len(char_indices)
+    num_classes = len(char_indices)
+    learning_rate = 0.1
+
+    print("Text length: %s" % len(text))
     print("Character set length: %s" % data_dim)    # length check
-    print("Dataset X length: %s" % len(novel.x_data))    # dataset length check
-    print("Dataset Y length: %s" % len(novel.y_data))
+    print("Dataset X length: %s" % len(x_data))    # dataset shape check
+    print("Dataset Y length: %s" % len(y_data))
 
-    batch_size = len(novel.x_data)
+    batch_size = len(x_data)
 
     X = tf.placeholder(tf.int32, [None, seq_len])
     Y = tf.placeholder(tf.int32, [None, seq_len])
 
     # One-hot encoding
     X_one_hot = tf.one_hot(X, num_classes)
-    print(X_one_hot)
 
     # Make a lstm cell with hidden_size (each unit output vector size)
-    def lstm_cell():
-        cell = rnn.BasicLSTMCell(hidden_size, state_is_tuple=True)
-        return cell
-
-    multi_cells = rnn.MultiRNNCell([lstm_cell() for _ in range(2)], state_is_tuple=True)
+    lstm = lstm_cell(hidden_size)
+    multi_cells = rnn.MultiRNNCell([lstm for _ in range(2)], state_is_tuple=True)
 
     # outputs: unfolding size x hidden size, state = hidden size
-    outputs, _states =  tf.nn.dynamic_rnn(multi_cells, X_one_hot, dtype=tf.float32)
+    outputs, _states = tf.nn.dynamic_rnn(multi_cells, X_one_hot, dtype=tf.float32)
 
     # FC layer
     X_for_fc = tf.reshape(outputs, [-1, hidden_size])
-    outputs = tf.contrib.layers.fully_connected(X_for_fc, num_classes, activation_fn=None)
+    outputs = fully_connected(X_for_fc, num_classes, activation_fn=None)
 
     # reshape out for sequence_loss
     outputs = tf.reshape(outputs, [batch_size, seq_len, num_classes])
@@ -91,18 +131,36 @@ if __name__ == "__main__":
     # All weights are 1 (equal weights)
     weights = tf.ones([batch_size, seq_len])
 
-    sequence_loss = tf.contrib.seq2seq.sequence_loss(
-        logits=outputs, targets=Y, weights=weights)
-    mean_loss = tf.reduce_mean(sequence_loss)
+    loss = sequence_loss(logits=outputs, targets=Y, weights=weights)
+    mean_loss = tf.reduce_mean(loss)
     train_op = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(mean_loss)
 
     sess = tf.Session()
+    
+    
+    
     sess.run(tf.global_variables_initializer())
+    print('=' * 20, "{:^20}".format("Training Start"), '=' * 20)
 
-    for i in range(500):
+    iter_loss = []
+    elapsed_loss = []
+    
+    start = time.process_time()
+    for i in range(10000):
         _, l, results = sess.run(
-            [train_op, mean_loss, outputs], feed_dict={X: novel.x_data, Y: novel.y_data})
+            [train_op, mean_loss, outputs], feed_dict={X: x_data, Y: y_data})
         for j, result in enumerate(results):
             index = np.argmax(result, axis=1)
-        if i % 10 is 0:
-            print(i, j, ''.join([novel.indices_char[t] for t in index]), l)
+            
+            if i % 100 == 0 and j == 0:
+                print("\n Step", i, "Iter", j, ':',
+                      ''.join([indices_char[t] for t in index]))
+                print('Loss:', l)
+                end = time.process_time()
+                elapsed(start, end)
+        iter_loss.append([i, l])
+        elapsed_loss.append([start-end, l])
+
+    print('=' * 20, "{:^20}".format("Training Complete"), '=' * 20)
+
+    output = predict(result, x_data, outputs, char_indices, indices_char)
