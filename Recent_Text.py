bc702f56a36aea1a65634934a65261569d582923
Liquorshotz <jungwon1413@gmail.com>
2018-04-04 23:59:26 +0900

    Add files via upload

---
+++ Recent_Text.py
@@ -0,0 +1,1387 @@
+t 
+r  suml_
+ou
+s..s
+
+uls(,usp.
+_),rfla,)l p,em es_er_,
+lfp
+ale_p__suss)
+poss=ens,e(a_a(tm('
+b
+p
+lss
+eso(tto_,es )p
+s
+
+p . aa)e,f(l
+t
+_al,hso,us _,e
+,s,)u_oc,cf,e
+r.af.
+,n'
+n(aat[
+,
+,or
+e
+
+ 
+a
+a
+ite_
+n als
+(t'lo_
+t
+
+,t
+,a 
+)h,
+.bu
+plr
+et)s
+
+)(opu
+ 
+onapus
+u,otaa='rfat,spop(lep
+
+__rs r .
+a 
+r
+s()
+_s)opo 
+rap(
+it_ea
+
+co
+cses,'
+.ds_o
+sser  a,(s[usr'. r_t t (rlra
+.r.
+ t) 
+ssa n er
+ss,(eal)t)_,
+ ,ur
+r
+
+t
+'a _,
+s)_s( ts oispap
+t(
+ lta
+,
+lf r..)t_a,t spun.u)r(
+ e,s_f i_ la s
+_ac't
+es
+t_,.
+rl
+e_r laa,
+_(,.
+ _(_,.r
+on)s
+fe_t._a
+r(er r
+r.rar
+f)t)a,
+ s)u(.u)r(r l_lb
+s.
+)trl=ea et,,a e
+_)
+ax lm._c.at a
+(l,
+ion(u(r
+s('oa()en[st
+errs_)tpre( apau.u_t lab tr,
+)s,ra.
+u.(e_
+,v
+)o,ea,ta )t_t t:a
+ r ,_=
+rfllr
+f(c't_ef(sp,
+r)sdf(ia)so
+_s
+p
+ (c.atsoer)ae
+_apt.o,)t ler,
+p la(
+ss)ald r.f)tap)
+.
+,eA_(s._p_f_p..)t_oe
+,'eo)e(a
+t,
+s'uf
+ s.f)t)l,ts
+).s a )
+(ss, s
+s,,o_ss
+)a.r s(_otet,e't ep(s)f_ae
+s)eerar.rc_(too,sss)op.u(ralat apoun) rr
+ sbi
+(caoe
+_
+__t,tr)em(p. ,ral s'tcs'rfn.r r(_)rao,t_
+uuf.e(t[per_ea.rase_s,fft ao
+)tap)t,
+pop.,mr(b_es)aon e,u_l)a_)s(a
+elou._)r_r_i.i . oa  l fcf
+t_ tt
+[ems
+(t
+prefo'l_(
+t_ r_o
+u_oe_p.y,(eol _ss,ef.e_c.s 'sab_artot   
+tn er
+  .r nee
+ a,as
+,(r,
+ai,ps'sos(
+_pms,(,r._ ffol
+ rf
+(rri
+le raotdle.
+,s,sr.
+(s,oe( 
+emp (t_(f'no,(s
+nrt    o
+ra. t t( or .ue(ae
+,
+pus
+u_l,s(escdpmno
+_,ms[tsrmraa._(taoe
+ss)opon)t p,t
+(_s
+uf(c,
+t(,)sora 'r f.r iaa lrat t 
+   rf ._cr
+a
+ cl   ae
+_)t)t,
+ apop.l_r
+l(e'b_oauts .otla 
+(pln_r rfrnr t . arf  
+ . ,
+tl o y.  itb_b_bp tss,,
+
+ a
+foo,atro
+er_ea(c_)
+mlas eu
+ec_,s)_as,'(='r
+b.r t r t (a t e t r ir
+t
+
+ t cl l
+,)
+)b t
+_p.p(
+eroe(t t_oauts .otla 
+(pln_r rfrnr t . arf  
+ . ,
+tl o y.
+o=e)tro_ buet eserfrsae(e
+(e_let.e)odat() s flp.r.s(l.asn.)
+(
+e_s
+nsb.pot)))e t,(r,
+a,,ars
+
+ces( r(,(eapen.
+mol
+u(f_fs)a.a y sol.amu. t
+(l_
+,t_
+ ror  a
+
+t,oo
+p 
+,es,
+a_su.
+,l
+.)t[l(t.t)
+:a[(r lau r l tclfpsn  t
+_et
+
+
+_e
+_,ns t t
+oe,t
+r.n.r
+ 
+_ef(o ssfar r(_(l.
+t_uet..)srou(t(as)p.n.r ra,
+_euuseslo
+s
+sn f=-ar_l tsla.
+.re o=ep e,t,bsalsu_l.s bf)r.
+pphn_r r
+feamlsp
+.ss
+
+os_a
+atit
+r
+
+c,s)(
+,
+souo
+ero)tr)o(
+at
+)aap(t
+
+
+spu_
+r,sr'r,er orfnft.t to(m 
+i's_s
+ e,
+._
+_r)r 
+a_,
+,t
+
+ 
+mmt,rmlerb
+ua
+ts,(ero_a
+(ss
+f
+op[su.f r 
+a
+ar_o,b
+
+)sao_,a(t o,fsal,
+s
+)aa
+,sl .p.
+
+,a)(u,o_,nos r[,(
+'
+(_.
+_a_oy
+ttf{sp
+s,p,sr)l_er orfnft.t t ll(
+ t='s
+_ r.oa
+.f.
+
+
+_er)e.
+pl
+r)))o
+o,s,
+_ea,s ,l_s(_ ro. b
+tp eamto.rou)
+)ret
+ _
+of_p)
+,boa 
+t..s )t f(s(t_,ts.r,ts)
+su,es,
+_o t
+),a, afo  .e',,_erfn tar 
+y ,osiap(_aper )sr e
+o_(t'tet
+ol
+uf r.a t o_l
+
+r
+_dnl r_. sterb_,m
+
+ppy_,em,,pnf
+a,aml .)esl)ss
+ifss,fln)psua,f.l)
+
+..msr
+elf seuen oe
+t os
+
+)sr,bp utpe ar tso ss)a 
+ ra
+
+
+(t)o
+tt)et a )r=e(tppa
+_ea_b t
+('t o,t
+.(tr
+ 
+ p)un.leror)_)e 
+laos,
+r
+r ra_acolt
+'
+fop,b b )r
+lar tp,
+s,rs,et
+o_se.c
+m
+s),tams(o tr'
+
+_b.r d 
+.a,s
+e
+_,r_t.
+s
+'t o 
+ms
+
+a)o,,  r)r)a)e o
+)
+i
+.r,t t sstro p)=ea
+r,.s
+or[f t r i
+)aa,o b
+f_ppao
+,
+_
+)_os)uf(s'l(ocmf
+o ar,lmtaa
+rt ur(flet=os)
+t
+s)o
+_es))r'
+s 
+ 
+,,sn
+aft l ta_ nes,ap aeyb
+s,lc pi
+ r eria
+o.
+t)
+es'aea 
+a
+_p aa_ouos t l
+os
+)o.r r t 
+csur)'s lfs fec.)c)tea
+'rou
+rerbe(t
+oas
+(e
+aalosspasl,r smc.et
+'a.t r.t c
+,(tr)f
+)a,l 
+_tf
+pe
+)es
+,t,u
+_.,b,tp,(trce(
+or)l(up )%t.c)
+)_)
+)t ol)os (r rtroet_)e)tfs 
+i
+b_
+r'ep,a
+.mssu
+,)p,(tr
+es)as
+l
+ers.(tao,(,spup(i'seran.so_)tp
+fb,t
+ 
+n( s.)o.ata
+(t
+'__m
+bs)
+,r ((frrlr
+r
+opltm,e,r(l as,)
+seua
+)p )et
+ 'sn,.fm.atal_tss,t,,tr'als,ar
+s')of fltesosp.ssp_o_,a_)eai,ts_
+a[
+eft tfs.api,ap,f_s,e),)p)u)b,r
+_
+,
+_)ets,-_u
+ol
+al_t,_nl t    a la. _bbs_f)d(uss,ar r(tf
+
+p
+(u ( )s  es
+ea,
+aop.sps.an )ts,(tro_es,ts,_sbla= rar,s)t
+ets
+)[ee n tfi .r s us,,s  )s m
+ar)t s ar(l
+
+m_
+tp.p
+t )_sbuas',et,o .a_t r.r,sffmf,r se'
+opap
+l
+m s,)ft r,s s.etuar 
+ b es,(toles'a.ep)t t 
+
+
+r  ,te)t(a b,(ppu t )r(pfuee
+em bts
+.y
+r'rur(ol
+
+ tr
+e
+,[,hp.(_
+t,spor
+ec tr'
+lc r m
+asm))t
+,,r a((p ar_ea_
+etml,ts
+(,)t
+od
+p= s( es,_s_)es, s  
+r(r em )b,ouofoo
+)s
+r
+ bos ria
+tt
+ospu_,r)
+s,
+sob,epul(f)o,sl(.fc r.m.o s
+ap t_ts(t') .bsea
+
+
+tsl ,
+ ( )
+)o
+'e=
+a( es.,ss,(c't,ea
+ess
+)
+
+laiu.r r,
+au
+u
+eaifltts
+. 
+cp)s_ss)
+_)
+e_o
+ts 
+
+er
+e3 t)o.c.r trss(s ts )u(si_b_
+,o
+ t
+l[,so
+(,)b,e
+)l]ee
+n'u
+spup(s r,_a spop
+popuarer,t
+)si
+ t
+_a
+[lila_s
+,y(t eies,t
+,)b,erar em
+e( sp)l.
+'ppan r,tc.
+t_)la,te)t)(ta
+,t
+,,
+,(re.)t
+),)ter'r t
+ ts)e
+),p)u_a or
+per.r sof)t)t,t 
+)__s,fs)a((s_e os),at)ua
+,usss-a .ae eaaf
+oe ospo
+,p)om,r,esr a,t tar,t 
+)t peaa_f)a,tseasse_o ts[ts,'p 
+s,e,s),f
+u,eamltso er .is _eaau,)sp.(
+'taa,t  a
+t[,a _.m opau(bota'_ar r iol(tapuso,_p,
+m((p
+
+t)f
+s
+aob o
+
+r
+
+s,a.
+lrb
+e
+
+aleb ospe_sr)t
+,s)s(
+sa__esss'f
+t t.ss
+
+s 
+t(s,,a '( bs,t_,r f
+so)b_,t
+t_s,a.narrsll'(ob e,'st )t(s,)asoa n
+,t
+,r e(so)op
+a
+(oe t)'s_ t[l r tm y.
+.s.r(_(t'o_o_'
+fs
+s)
+te_,
+_)
+)
+ .a,
+t,
+
+se:ob ())tt . t( e_s,.(
+ r n
+t
+t f  alrl(ae,ta f f tara_itf
+t: .  et, 
+
+b er _i_r)(s f n r t l t i   fr.ia  np ysn a.t ld
+t r bt o,
+so)
+_'_el,e(,se )t_s.a.
+ s to_(t'o(a tr,os,e=)em_,sr(t)s
+,_s
+s,ssii_on o_'t_,. ec]auss_
+aob o,
+e: st_.l
+(s rmtrr    p y ltlr
+nn .ftf f 
+
+i e r frtt 
+
+ _ rr u ,rrrrrlf(ctlus(a)o,s oa t r t tylr.r r lt_ t .fa
+ t lt
+r af
+tlrr'
+ ts 
+
+tl uuts,_ o'l(s='f_carar t(lf la  
+ l. ilrbeis_    ni(ttr
+(fo
+o op et,or,ttss(etss 
+_s[f.y r.s'(erao
+ s (ts ))ouor'r)em
+s,ss(.(
+a
+e
+,bp,)s,taesmn'__sssulf r t ao,atalr_t
+(lue
+t )t,lia
+ol e,_
+W o
+mp)e_[,lrlr o ro_)trree(
+pse
+_)s
+p
+(eaaoutnop
+m,
+
+o(eafntr.sh s)esfa(ss,h=
+e(t)b
+t e (e
+t,ta)
+.b t
+c
+e
+p.l j e ,t=o
+a
+)us,s,(s
+r')o
+l
+,r ir r 
+ t=
+j(tmas,el=e_t
+)rs
+ 
+c[(a
+e
+ e
+,a
+,_
+
+ s,)r=ests)tsol
+r
+_)
+)a
+utr[pf)_
+so_ r .rty
+)pe(fe 
+
+
+ra
+,as'ob,e
+'ra,
+l
+ss,
+per
+e
+t
+)
+=e _soe(tro.asi)fn e
+
+(t(r_(a ti)
+)(()ea
+
+
+estr j_W=e
+b o,
+
+
+p.t
+bs)er:es,  rs 
+pon=f t
+ 
+
+ r t r (y 
+
+ lr 
+ 
+r, 
+f
+i
+
+ r
+_d ly :b
+b,be_r
+p(o
+ 
+,t)sif n r,ra,p._(t,b_ts,)p,r_
+)t(oefb_s)o_o
+ 
+s_tl_fts,ap.l(tr a s'ffl t r na
+lfr
+tfe  a'a    erpn 
+d.
+ 
+
+fb
+
+ps
+
+n.u
+t(s,s_eso,
+sa.p se.an'
+,s,))s__er  lpf)t)rosta  a_or
+
+)t)p fm_e_a,t
+,.t s( e:
+or
+_,lel_tn e
+e)p
+afp 
+
+_u.d
+r,r 
+_'n}ti_l)ao
+ u 
+
+
+)o,t
+ o
+_,s'_)n o f rsu 
+r))r
+),_() 'uai
+ea t 'roui
+l)
+ 
+
+ u
+,
+
+,
+r,(t_pa_'))p,
+,sob ts erat.e t r 
+ar 
+
+.t ltta ,
+r   
+ tr a
+rat 
+
+octu,taae_
+deeas p a sfs
+,asera,e)tm( atuW lm(o es)s_ess,
+ts.a
+)aa,
+s ea_
+ os's)rfa  fs (
+'sum s,te
+t(  s:
+ssue
+ e)(sp
+b t
+)
+s,ts
+_b)t)t er,au(W 
+_ t t
+t t lr_rft.n
+d 
+ _
+,r
+t 
+ 
+nt
+nl llt .
+ 
+_re_sa
+apob,,sp at_aets )rul)sl,er r s eao_esl_sp,tse)tc_)le
+b,t
+
+_ae
+obt u(,
+ler)rs,s
+oet_os,sseans)mmftpt
+_[f re
+ e f la.nfttt,
+s
+s,t 
+,ss
+)s__.
+t
+i
+ os)es,n_,
+..t_
+aot eatauep= aulr o
+ss'u)sss'. 
+ s,)a,spo.
+e(t(t a
+u(ut_
+r'a
+o_)c[
+,t
+)pup(u.
+(t.(c)t_ea,_s,
+bflas_,fr sn_
+a
+uso(,t),a(br.)s_
+,'_lp.p
+
+(r)r.(t t
+ea'ts,.btlesn(sn,s_r 
+er,s,b.ssea_
+: u_
+l(s-r t,s( e
+rt ns
+fs'r 
+
+ra
+rt tar a_ lpfsu.mr,es o).n
+p_as)  r,es,ppo
+bs)_
+r t  r  
+=a_(las
+r,bsa,ut)od
+teaso es
+a)ruts.
+),er r(f
+a.r al.ep,
+s)st_)ea,ts,es
+la( as
+r ro_(c)
+_ea)ts..otlas(ran r 
+
+n
+raal.l),pr,st le
+s_
+d(u,aa.l'r
+l)c[)[taspu
+(soa,alf.(dals,,
+ sp rs_uar r
+c,tm(ts
+e)p(l.(trtems
+pis erlpafts'r.n r s,tm
+ss,e(e.p,as'_
+_h_
+'ry ato
+br_r 
+(s_ ,esa _
+s,atroe_s,)
+ (s
+}'tls l)t)b_o_ofet t(,ta
+,
+_ l,)sla t stas(
+ e_[
+(t(u(
+i
+pposss'
+ar( 
+a
+ea)e)) ra_ s,s)el,op(.oeli 
+'[)o.t
+iep r(
+.t e
+m[ror_rst t s_(_s,u_
+s
+,s
+o,'s_er r o lms,e)td,la oa,osa,a s
+t
+sle_
+ os,
+r)
+r
+.la  tmieb_ta(c'.ab ma,osorau
+p[
+ln,
+_
+t,tr)sr[.
+y s t
+'e((s,t,(t)o
+
+tr(pt__,
+es
+e(e:a
+b,ap,t t
+er,es
+n r=
+afr t,t talst
+
+_)
+'
+_
+
+
+o
+' _alf)e[)_ts
+srol
+  _o
+(t_
+t_ta 
+
+ ur(per, p(_tats's)
+
+et
+rd,o
+)t[s,_s
+
+)p(l_ar)
+sp e
+
+e(er
+,e
+,
+s,m(,ls,es
+
+fl e i aa(r
+ fr.a esea
+a
+el'o opot o
+ (a)
+[
+
+p'
+r_
+ rt_,_)i (r l(_s l
+t ),tser
+at
+_aa.l
+
+__e
+,ratyt
+m
+[s(t s  rfa_b:er= rf
+am tp)es[er.a s,)pol_r,_a. tis)t
+,
+)
+.
+ r r,._.y.s
+e oe(,sr,
+_a_b_ol(
+
+tm
+si_ 
+ ta
+p aon,ts
+oe s s )_(f,bbs,_suaufl(pl,es
+y))
+'
+,titr, e(rlalelooa
+,s,t
+set  a:o,s,sl 
+c,era_s._r ls f.er ls's,,r 
++
+ s'
+s 'lfnfr t r
+ttfa
+_
+  (ao
+ t,
+sb,sel
+(r ._ls
+eses
+ar.t(s(_,t a(ces'(or or,
+_r t l .a 
+rrd,a bmrsr_ussap ,e_l,as a e,apof(u
+r_b.(e opea _se.onla
+[rrnfrdalrf,i.. noia c
+,ta_
+_,p=jes
+o.ay
+
+es,olosspap.
+laor=u e. ros([[ut),s,ar)t,trol
+erbr
+,s,)r .
+ses_sa)ll, t,'l)n y_t
+
+ l ia f
+rf
+
+,  ae_o_
+_bfs,
+
+_l.(s a,t spup.
+(rfr,(p,o_ss,ts .su((f(
+rni
+talrf,i.. noimfa
+,__p
+oas,o.ap(pts)er sssa.t s
+
+t_)e(e._ s (=a (p e [al_cts,)p,l(
+pfe(aad)u  _)s
+o,,tr.(l
+sb,ep._
+.rt, a
+s,'p)f(s)t_ r)csma ep,(t
+)ao
+ r r r t t r e l
+ t rr'tt
+,es( es'
+(  s
+_ur_lpo
+s'an 
+]seter,ot ts,
+sn ufl
+tfot b )t(t(t
+ _
+m_,,sa.at
+o
+ts)et 
+sipp
+a_f(' r
+_er,o_(e(ler(
+ 
+s,ofbs'f_bletso,ts,o,
+a'r ,p(
+,r rrs.(e(t(
+ lsp
+o,es,t_'so_er
+e
+(yt 
+aa_e
+:es'
+,op,t,)us osn_f,r i tan 'r u
+e)ob
+ul_te)_bp))se()o
+)e_a ap
+ts,'
+'s,
+so
+_, ray s,rs) m ar)l_
+_n.es
+)esseat_
+'.fap)
+ t er
+y t t r . aad
+ ) y fa
+n
+a r_
+faeol ts''o
+ml_s=o
+ t t
+l
+t l f
+ n yae f ni e rss (eaotsuo
+eaoa os (m
+_
+_(un rao s.)tflla)'a ,e,s a_b)(,r (
+tr)n_( 
+
+p ta
+_a
+sp,
+o
+o,a,essotiler s,
+ts
+ffy.s 
+ s(ts,a
+ot)t)b,s
+ue( ep,
+t,p'aa t)p,t)s)(,.n r p,
+r_lib,ub o,,ts,fo
+t _l
+o,tp 
+ser (
+_t spou)
+lpopuer r tn,.(t_p
+
+ fr,at_f.rtl(ltfea
+uc)'aos['l y
+sfr e,b er ra[,
+ t),_ss ,psl)_tss_(eop tf
+p,t b
+erfn_t t l e l,.fr( 
+r
+ a .rlt r f   t.)t_flan)s.'
+ft l i
+l  
+rfrdllre  r r r rntprtt o_(co
+(tfo
+)l(i_
+r'r n t r t l f
+ n yae f ni e rer   las,)taiesso ap.b
+s
+ la),[ slperao s  tsre (nl_s)estml,t(ue[e
+l,, pa (
+e.lt
+_
+ os'
+(
+ta[,(6aapersas,
+
+),
+.t rrrlrti )r
+lalt)a
+os(ss).res
+=ea)eaas't(_es
+afbs
+_sp
+rtms's a
+,r rb (u tams
+
+dl.yar t rbselae)t lea,e_e
+(s,
+tm,_[[ l)r)
+ c,fcs'_
+y.t t taseral)r),
+tb,
+,_es,s
+e
+
+
+c.(t oe(,al(uo
+er=
+
+)
+ ,t_ _sul_[
+ar.
+_n r.
+,
+ tc)e,so.a
+s,ot)_t_o
+ap.(t
+o_'s(,spu_)ss)
+.a r r ar(lib)ut
+ou
+tb e.oio
+_a 
+ ss
+ts,uf
+,oumt
+sf,sl_.rls
+se,a[tn'e_
+
+bm
+oubl'
+ ot
+o
+_sl aft
+tt_t(bop
+bss,tr.( ero,tr'a
+
+(,s(
+,ol,ro
+f
+te.at
+
+
+ b,ett'_ar
+)mrlt n,r _orp s.y
+se(ot
+p-(oa(u
+r r aosts
+p,s(lssus
+pstaf
+ l  cf(t)l[_,e)e_f(),,
+r_r r
+lat[ll__s,e
+.e(t,
+f,ur''ssr f o_so
+)tolf
+'us(o(
+e)p(o t s_
+iot l
+ r ynf
+_)oe
+ 'r)u(e)t(lat t
+outs,
+alt(ot.)ta't
+,es,.an
+af)s s a
+)e,) l
+_ ,a.rfr.f)c)te( ap,ts,)u's
+t t(f(a ep 
+s,oua ,f,
+pnrr
+.la,oo(p,sp)
+) ss'_
+t)tan
+r,(rus) msoa
+o .rl
+,s lt.o.ec
+(r_
+
+(t_tf)tr)ou
+s,sn.e[c fa e_s b 
+_sufr e t..(t_ler,es-)
+fr,iass,
+r__i 
+u(sl)p
+e_p(_),e),.sb
+ a,t
+(_ a
+e .sl,t_r_a eol)t[u
+s[spfp_e
+)oso.(tr)oe
+sfs_'e)s.
+ a
+  st s 
+.)t),o_'st
+_,t)
+
+_)sp a,t r_ta_eal)tsser
+_(p)teo,[ oto.atr 
+s, p)unos
+))s),
+_os),._t t[).t r a ff(r
+l  a  
+,
+r.aer(ase
+pWososp
+flt e t
+(a,e ).
+ear(e(s.)o,t,a)+[ol
+(
+
+t,)c u(l
+)s t=or_rou)t'lea 'p
+(s,',
+s't__
+r ef, .t_)tr)ot ,
+ t=ear(pof(tr
+et,)_,t ea
+msel
+)l,)t_(tu_s)sn.ut_r,t ct_osperm ,,sst
+
+.sr_t)b,m,sr.(sy a.t 
+,ta t
+s,e(t_
+(,)so,bs
+su)let
+_u_s a us_
+mtl
+ts( rb e(,ts,aes)er,_
+'fsnra
+s'r 
+
+r te
+(,e
+p,ea e()ose(e
+tpa[et
+us,(t)s)_ .n r r
+
+)c
+uat[spal(e.)o
+t t
+s)
+r)perls t ic_
+ssor
+ t s)'ioc)ott t s)e_
+sp r
+t l,_s,)ls 
+b,ap)soti_o_s
+n a.b 
+r u
+[els
+s[s
+o) 
+[ m_o o
+ti
+t l
+t.m t,_ a  f
+,ciafnfr.sr laretu_ t e)smr ra
+_t'l
+t
+ a)auem,o)an es= ri(
+t_,_s()ps'__)s[
+aln.ams)uapmsosesea_is s,e_u
+,aa a,olsl_as
+alb._
+_se.b(fsa(_ e
+e( mr rfp
+ef,p
+e
+u 
+ss't(talp
+
+=e
+)a[petst)taf(_ep))a 
+ a_.l_(,ap,tt_'u.
+o
+
+
+a tfls)a.b r(
+_s,e(p_. 
+p,(s
+o)os'rt
+a,or l
+oaa,e
+as )l,, ssa.f rtr '(..auas)un 
+,eree
+ausoa ea)
+= r,t,(_ .ec
+
+,ot,  pr)[)uao
+
+ua,(p es,'la
+ou
+tp
+t)la
+'a
+s[su.t
+,ms[e)uer,a,er)ts_
+ ,,ef)p,ol ea
+i ssusp euu)e_ao.tua_  p 
+sa')'_)_
+s'_(a
+ u_so_fsse
+b(,_u(,,t es asap
+'s)tus'ussf__o.)ra[.e
+_=
+sts)u)bnp
+ts,_t,ts)mb
+b_se.c,c
+s'fta
+.(t tr,e,er,ar c,
+_ub(ase='re
+
+o
+ssals
+efs
+,
+tt,o
+,taon[
+ap
+ 
+s
+flf ot)ea'
+(t'
+[(_ast_ea
+s.a_ones_osuft['
+'rr tr
+ .,t,_ aoi er_,
+bt p=,sn_,,p
+(
+oi
+t
+ t,acs r
+n.r os,(st s,)c m (r eabbr
+sp,
+,(s
+.t b_aa,
+_
+_s,f_a
+m
+t e et_la 
+
+orta
+f
+s),_c,es)
+e
+(,rer
+(se
+p
+ or,
+ t r a darr
+at (l e s( r l.r let,)er t
+,ssl on rfd'
+ 
+e,),ep,ts[s[
+t_rat.ar(lib,tau_[s'p'ras(_ t)tr
+ut,os'a.n.r ts,ea(l)r(
+r
+t_ . a_aa,i,
+')p.r
+s,fts'a.b rfs(_t_
+r)t'ens)ut
+aspes_,t a 'r
+
+(
+__
+_
+tsuaadts,s 
+afystrtyn
+e_
+ob te,ts,ts
+
+b
+b_a
+)t,m,b,_)( e
+,es)y'
+(u:'r_t misif.soepya
+
+s
+ tc,t
+on
+
+ap.fss
+fln ot)ea'
+(t'
+[(_ast_ea
+s.,_(b,u erbn'up
+'l
+y '
+e
+e[uater o
+ptr
+t,
+ss'r.y.r 
+ ees
+,
+),s t 
+r eabbr
+sp,
+,(s
+.t b_aa,
+_
+,o,_.
+.ar,(r)l'r(
+=
+rf
+se
+[
+_ir r_e 
+
+t f .  
+ i
+
+ ra(r'
+ft seas
+ at,raa,m
+
+r (ee. r),
+t,t_o [s)p'rlnepfre sp(u_,(s)_.y r
+t
+,er(r)t'
+
+sos .sa sa,t
+rm)
+,r s,fts'a.b rfs(_t_
+r)t'ens)ut
+aspes_,t a ,_ 
+b
+t_o'
+b,bf
